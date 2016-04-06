978b25312e90360737a6bbf089e858e47acb1d69
Luis Bausá López <luis.bausalopez@vu.nl>
2016-04-06 11:45:01 +0300

    add popup and comments

--- js/map.js
+++ js/map.js
@@ -8,16 +8,15 @@ var blOptions = {
 	minZoom: 1
 }
 var baselayer = L.tileLayer("http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png", blOptions);
-
 //Map options
 var mapOptions = {
-	"center": [53, -1],
+	"center": [53, -1],		// latitude, longitude
 	"zoom": 13,
-	"minZoom": 0,
+	"minZoom": 1,
 	"maxZoom": 22,
 	"zoomControl": true,
 	//"crs": mapCrs,
-	"layers": baselayer,
+	"layers": baselayer,	// in order to load the map you need to pass it at least one layer, which will be a basemap layer. You can also pass it multiple layers or add them later.
 	"attributionControl": true,
 	"fadeAnimation": true,
 	"zoomAnimation": true,
@@ -35,10 +34,10 @@ var fLayerOpts = {
 	zoomToBoundsOnClick: true,     // default true
 	removeOutsideVisibleBounds: true,   // true for enhanced performance
 	animateAddingMarkers: true,    // default true
-	disableClusteringAtZoom: 12,    // default disabled
-	maxClusterRadius: 100, // Default 80
+	disableClusteringAtZoom: 13,    // default disabled - HERE IT IS WHERE WE DEFINE AT WHICH ZOOM LEVEL WE START CLUSTERING MARKERS
+	maxClusterRadius: 100, // Default 80 - HERE WE DEFINE THE RADIUS OF AN INDIVIDUAL CLUSTER
 	spiderfyDistanceMultiplier: 100, // default 1
-	polygonOptions: {
+	polygonOptions: {	// THIS IS THE STYLE OF THE POLYGON OF THE CONVEX HULL OF THE CLUSTERED MARKERS
 		color: 'yellow',
 		weight: 10,
 		opacity: 0.9,
@@ -48,9 +47,9 @@ var fLayerOpts = {
 //                    return new L.DivIcon({ html: '<b>' + cluster.getChildCount() + '</b>' });
 //                },
 	singleMarkerMode: false,     // default false
-	attribution: 'bla bla',
-	maxZoom: 25,
-	minZoom: 1
+	attribution: 'FEATURES TO BE CLUSTERED - Luis & Pedro',
+	maxZoom: 22,
+	minZoom: 4
 };
 
 var featureLayer = L.markerClusterGroup(fLayerOpts);
@@ -64,13 +63,12 @@ var oneFeature = {
 	type: "Feature",
 	properties: {
 		name: "First Feature",
-		alt: "",
-		title: ""
+		description: "jojojojo"
 	},
 	geometry: {
 		type: "Point",
 		crs: 4326,
-		coordinates: [-1, 53]
+		coordinates: [-1, 53]	// longitude, latitude
 	},
 	style: {
 		icon: {
@@ -80,20 +78,19 @@ var oneFeature = {
 		}
 	},
 	options: {
-		
-	}
+	},
+	popupTemplate: "<div>{name}<br>{description}</div>"
 };
 var otherFeature = {
 	type: "Feature",
 	properties: {
-		name: "First Feature",
-		alt: "",
-		title: ""
+		name: "Second Feature",
+		description: "jijijijiji"
 	},
 	geometry: {
 		type: "Point",
 		crs: 4326,
-		coordinates: [-1.01, 53.01]
+		coordinates: [-1.01, 53.01]		// longitude, latitude
 	},
 	style: {
 		icon: {
@@ -102,9 +99,8 @@ var otherFeature = {
 			iconAnchor: [9, 9]
 		}
 	},
-	options: {
-		
-	}
+	options: {},
+	popupTemplate: "<div>{name}<br>{description}</div>"
 };
 var feature = L.geoJson.css(oneFeature, featureOptions);
 feature.addTo(featureLayer);
